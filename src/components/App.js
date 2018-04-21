3b2c7c2fad7ea553b5ddd10c15b70f04096d1628
Felipe Venegas <phil.venegas@gmail.com>
2018-04-21 11:12:43 +1000

    Added props and propTypes to Header

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header title="Welcome to React baby!"/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import logo from '../logo.svg';
 import './Header.css';
 
 class Header extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired
+  }
+
   render() {
     return (
       <header className="Header">
         <img src={logo} className="Header-logo" alt="logo" />
-        <h1 className="Header-title">Welcome to React</h1>
+        <h1 className="Header-title">
+          {this.props.title}
+        </h1>
       </header>
     );
   }
