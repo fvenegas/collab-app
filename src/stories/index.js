d1c0a2ac53e866a7b327958208ec728489dbede4
Felipe Venegas <phil.venegas@gmail.com>
2018-04-23 22:22:28 +1000

    Added Header stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import Header from '../components/Header';
+import '../index.css';
+import '../components/App.css';
 
-import { Button, Welcome } from '@storybook/react/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
+storiesOf('Header', module)
+  .add('with title', () => (
+    <div className="App">
+      <Header title="I set the title" />
+    </div>
+  ))
+  .add('without title', () => (
+    <div className="App">
+      <Header />
+    </div>
   ));
